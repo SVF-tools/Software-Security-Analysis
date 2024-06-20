bd8680a90c22b001f1fde46b6f9610a6d6f84ff5
Yulei Sui <7608399+yuleisui@users.noreply.github.com>
2024-06-20 22:38:08 +1000

    Update Assignment-2.cpp

--- Assignment-2/Assignment-2.cpp
+++ Assignment-2/Assignment-2.cpp
@@ -35,7 +35,7 @@ using namespace z3;
 
 /// TODO: Implement your context-sensitive ICFG traversal here to traverse each program path (once for any loop) from
 /// You will need to collect each path from src node to snk node and then add the path to the `paths` set by
-/// calling the `collectAndTranslatePath` method which is then trigger the path translation. 
+/// calling the `collectAndTranslatePath` method, in which translatePath method is called. 
 /// This implementation, slightly different from Assignment-1, requires ICFGNode* as the first argument.
 void SSE::reachability(const ICFGEdge* curEdge, const ICFGNode* snk) {
 	/// TODO: your code starts from here
