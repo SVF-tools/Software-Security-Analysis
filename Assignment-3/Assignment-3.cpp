e541a2235d7a01bf2fbc36761f87564c3e3638f4
yuleisui <rockysui@gmail.com>
2024-07-29 19:12:17 +1000

    Assignment-3: values of main arguments initialised as \top

--- Assignment-3/Assignment-3.cpp
+++ Assignment-3/Assignment-3.cpp
@@ -299,6 +299,11 @@ void AbstractExecution::analyse() {
 
 	// Process the main function if it exists
 	if (const SVFFunction* fun = svfir->getModule()->getSVFFunction("main")) {
+		// arguments of main are initialised as \top to represent all possible inputs
+		for (u32_t i = 0; i < fun->arg_size(); ++i) {
+			AEState& as = getAbsStateFromTrace(icfg->getGlobalICFGNode());
+			as[svfir->getValueNode(fun->getArg(i))] = IntervalValue::top();
+		}
 		ICFGWTO* wto = funcToWTO[fun];
 		handleWTOComponents(wto->getWTOComponents());
 	}
