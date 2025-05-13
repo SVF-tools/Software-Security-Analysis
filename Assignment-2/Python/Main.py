b18911875c8476713eecfe6db02e99a5e09db595
bjjwwang <bjjwwang@github.com>
2025-05-13 15:54:42 +1000

    sync with svf-python

--- Assignment-2/Python/Main.py
+++ Assignment-2/Python/Main.py
@@ -2,7 +2,7 @@ from Assignment2 import *
 
 if __name__ == "__main__":
     # check sys.argv and print friendly error message if not enough arguments
-    if len(sys.argv) >= 2:
+    if len(sys.argv) < 2:
         print("Usage: python3 Main.py <path-to-bc-file>")
         sys.exit(1)
     pysvf.buildSVFModule(sys.argv[1:])
